f7cc0c6b0bdefb23193c1786d6afaf3cf6e54392
forehead7 <stuart.ross.7@gmail.com>
2017-02-07 02:35:06 +0000

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,21 +2,21 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "1oDszV7lesCn0qTwGMEawsZIo9AhwQS7nsFXOreMWuZc";
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "1091-9502-4967";
+var inGameName = "Stu";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/forehead7";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://puu.sh/tRJb3/ae1f80328b.jpg";
